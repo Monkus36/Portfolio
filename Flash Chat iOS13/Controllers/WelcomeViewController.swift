6cb2ea3ed28bdbf9e85f409b094752d2c9d7be84
Monkus36 <33342765+Monkus36@users.noreply.github.com>
2023-08-10 10:17:37 -0400

    Add files via upload

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Cameron Mundorff 10/15/2022
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = K.appName
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) {
+                (timer) in self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
+    
+
+}
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,12 @@
+///
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Cameron Mundorff on 6/8/22.
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
