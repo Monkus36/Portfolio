1f39513082959a44554ff3b3c2b8714c8c7e1e20
Monkus36 <33342765+Monkus36@users.noreply.github.com>
2023-08-10 10:14:27 -0400

    Add files via upload

---
+++ Clima-iOS13-Fixed/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Cameron Mundorff on 4/25/22.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
