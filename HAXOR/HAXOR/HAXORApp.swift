311c9f17b10daddee9934a4f5c19664c71fd3de4
Monkus36 <33342765+Monkus36@users.noreply.github.com>
2023-08-10 10:26:07 -0400

    Add files via upload

---
+++ HAXOR/HAXOR/HAXORApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAXORApp.swift
+//  HAXOR
+//
+//  Created by Cameron Mundorff on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HAXORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HAXOR/HAXOR/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  HAXOR
+//
+//  Created by Cameron Mundorff on 6/30/22.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
